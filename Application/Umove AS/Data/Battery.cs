ac45eafec64fac328ad550bf319396f4e24a7cd7
ksje713777 <ksje71377@edu.ucl.dk>
2025-05-03 16:10:00 +0200

    Tilføjet relevante klasser i datalag og handlers i tråd med SOC

---
+++ Application/Umove AS/Data/Battery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Alle stamdata vedr batterier indeholdes her.
+
+namespace Umove_AS.Data
+{
+    internal class Battery
+    {
+    }
+}
---
+++ Application/Umove AS/Handler/BusHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//SOC "Separation of Concerns" - Handler håndterer alle metoder vedrørende Busser
+
+namespace Umove_AS.Handler
+{
+    internal class BusHandler
+    {
+    }
+}
