89416bd230540a593e5ba205bbdb549d0e35f837
ksje713777 <ksje71377@edu.ucl.dk>
2025-05-05 13:59:02 +0200

    Til at synce.

---
+++ Application/Umove AS/Models/Block.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Alle stamdata vedr vognløb indeholdes her
+
+namespace Umove_AS.Data
+{
+    internal class Block
+    {
+        DateTime blockStart;
+        DateTime blockEnd;
+        int blockID;
+        int blockLength;
+        int blockPause;
+        int routeLenghtInKm;
+        int blockTotalDistanceInKm;
+        int blockTotalTimeInService;
+        int blockTotalTime;
+    }
+}
---
+++ Application/Umove AS/Models/Weather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Alle stamdata vedr vejr indeholdes her. 
+
+namespace Umove_AS.Data
+{
+    internal class Weather
+    {
+        enum Season
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        }
+        private double _temperature; //Temperatur i grader Celsius
+        private bool _IsRaining; //True hvis det regner
+    }
+}
