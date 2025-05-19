2fd8ec38027d971721d0420445a0811a596fa1e3
Wuway <contactdieter@gmail.com>
2025-05-19 21:54:46 +0200

    Opdatering DCD i mappen slut artefakter opdatering UC3 Fikset RelayCommand til som den var før

--- Application/UMOVEWPF/Helpers/RelayCommand.cs
+++ Application/UMOVEWPF/Helpers/RelayCommand.cs
@@ -7,16 +7,26 @@ namespace UMOVEWPF.Helpers
     /// Implementering af ICommand, så man kan binde knapper til metoder i ViewModel.
     /// Gør det muligt at bruge kommandoer i MVVM.
     /// </summary>
-    public class RelayCommand
+    public class RelayCommand : ICommand //ICommand er intergreret i systmet.
     {
         private readonly Action<object> _execute;
         private readonly Predicate<object> _canExecute;
 
         /// <summary>
+        /// Opretter en ny RelayCommand Constructor
+        /// </summary>
+        /// <param name="execute">Metode der skal køres når kommandoen aktiveres</param>
+        /// <param name="canExecute">Valgfri metode der bestemmer om kommandoen kan aktiveres</param>
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
         /// Returnerer true hvis kommandoen må aktiveres
         /// </summary>
         public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter); //Hvis den er 0 kan den udføres, ellers ikke. God til brug med knapper.
-
         /// <summary>
         /// Udfører den tilknyttede metode
         /// </summary>
